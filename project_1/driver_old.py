c38dcf0d3135822d1b7d4d9ace2e24476949f759
Nimish Sanghi <nimish.sanghi@gmail.com>
2017-02-02 23:17:19 +0530

    added A* and IDA

--- project_1/driver_old.py
+++ project_1/driver_old.py
@@ -50,20 +50,12 @@ class PriorityQueue:
                 return task
         raise KeyError('pop from an empty priority queue')
 
-    def has_task(self, task):
-        return task in self.entry_finder
-
-    def is_not_empty(self):
-        return len(self.pq) > 0
-
-    def size(self):
-        return len(self.pq)
 
 
 
 class State:
 
-    def __init__(self, state, parent, move, path_cost):
+    def __init__(self, state, parent, move, path_cost=0):
         self.state = state
         self.parent = parent
         self.move_frm_parent = move
@@ -147,7 +139,7 @@ class State:
             new_state[blank_pos] = new_state[new_blank_pos]
             new_state[new_blank_pos] = 0
 
-            result.append(State(tuple(new_state), self, d, self.path_cost+1))
+            result.append(State(tuple(new_state), self, d))
 
         return result
 
@@ -169,7 +161,7 @@ def bfs(initial_state):
     st_time = time.clock()
 
     # node will have (board, depth)
-    frontier = deque([initial_state])
+    frontier = deque([(initial_state, 0)])
     # to have efficinet way to check for existence of a state in frontier
     visited_states = set()
     visited_states.add(initial_state)
@@ -179,18 +171,19 @@ def bfs(initial_state):
 
     while len(frontier) > 0:
         node = frontier.popleft()
+        state = node[0] # get the board
 
-        if node.is_goal_state():
+        if state.is_goal_state():
             # return (path to goal, cost_of_path, nodes expanded, fringe_size
             path_to_goal = []
-            cur_state = node
+            cur_state = state
             while cur_state.parent:
                 path_to_goal.append(cur_state.move_frm_parent)
                 cur_state = cur_state.parent
             path_to_goal = path_to_goal[::-1]
             cost_of_path = len(path_to_goal)
             fringe_size = len(frontier)
-            search_depth = node.path_cost
+            search_depth = node[1]
             runtime = time.clock() - st_time
             if not os.name == 'nt':
                 import resource
@@ -199,18 +192,19 @@ def bfs(initial_state):
                 max_ram_usage = None
 
             write_output(path_to_goal, cost_of_path, nodes_expanded, fringe_size, max_fringe_size,
-                         search_depth, max_search_depth, runtime, max_ram_usage)
+                     search_depth, max_search_depth, runtime, max_ram_usage)
             return
 
         # the node out of queue is not a goal and we need to expand it
-        nodes_expanded += 1
+        #nodes_expanded += 1
 
-        for neighbor in node.neighbors():
+        for neighbor in state.neighbors():
             if not neighbor in visited_states:
-                frontier.append(neighbor)
+                new_depth = node[1]+1
+                frontier.append((neighbor, new_depth))
                 visited_states.add(neighbor)
                 max_fringe_size = max(max_fringe_size, len(frontier))
-                max_search_depth = max(max_search_depth, neighbor.path_cost)
+                max_search_depth = max(max_search_depth, new_depth)
 
     print("ERROR: bfs should never reach this point")
 
@@ -220,7 +214,7 @@ def dfs(initial_state):
     st_time = time.clock()
 
     # node will have (board, depth)
-    frontier = deque([initial_state])
+    frontier = deque([(initial_state, 0)])
     # to have efficinet way to check for existence of a state in frontier
     visited_states = set()
     visited_states.add(initial_state)
@@ -230,18 +224,20 @@ def dfs(initial_state):
 
     while len(frontier) > 0:
         node = frontier.pop()
+        #print(node)
+        state = node[0] # get the board
 
-        if node.is_goal_state():
+        if state.is_goal_state():
             # return (path to goal, cost_of_path, nodes expanded, fringe_size
             path_to_goal = []
-            cur_state = node
-            while cur_state.parent:
+            cur_state = state
+            while cur_state.parent != None:
                 path_to_goal.append(cur_state.move_frm_parent)
                 cur_state = cur_state.parent
             path_to_goal = path_to_goal[::-1]
             cost_of_path = len(path_to_goal)
             fringe_size = len(frontier)
-            search_depth = node.path_cost
+            search_depth = node[1]
             runtime = time.clock() - st_time
             if not os.name == 'nt':
                 import resource
@@ -259,153 +255,18 @@ def dfs(initial_state):
 
         ## for dfs we need to reverse the neighbour list before inserstion so
         # that popping will result in UDLR order
-        for neighbor in node.neighbors()[::-1]:
+        for neighbor in state.neighbors()[::-1]:
 
             if not neighbor in visited_states:
-                frontier.append(neighbor)
+                new_depth = node[1]+1
+                frontier.append((neighbor, new_depth))
                 visited_states.add(neighbor)
                 max_fringe_size = max(max_fringe_size, len(frontier))
-                max_search_depth = max(max_search_depth, neighbor.path_cost)
+                max_search_depth = max(max_search_depth, new_depth)
 
     print("ERROR: dfs should never reach this point")
 
 
-def ast(initial_state):
-
-    # start time
-    st_time = time.clock()
-
-    # for A star frontier is a priority queue with ability
-    # to modify task priority in place
-    frontier = PriorityQueue()
-    manhat_dis = initial_state.manhattan_dis()
-    # node will have (board, depth/path_cost - i.e. g(n))
-    frontier.add_task(initial_state, manhat_dis)
-
-    # to have efficinet way to check for existence of a state in frontier
-    explored_states = set()
-    nodes_expanded = 0
-    max_fringe_size = -1
-    max_search_depth = -1
-
-    while frontier.is_not_empty() > 0:
-
-        node = frontier.pop_task()
-        explored_states.add(node)
-
-        if node.is_goal_state():
-            # return (path to goal, cost_of_path, nodes expanded, fringe_size
-            path_to_goal = []
-            cur_state = node
-            while cur_state.parent:
-                path_to_goal.append(cur_state.move_frm_parent)
-                cur_state = cur_state.parent
-            path_to_goal = path_to_goal[::-1]
-            cost_of_path = len(path_to_goal)
-            fringe_size = frontier.size()
-            search_depth = node.path_cost
-            runtime = time.clock() - st_time
-            if not os.name == 'nt':
-                import resource
-                max_ram_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss # get ru_maxrss
-            else:
-                max_ram_usage = None
-
-            write_output(path_to_goal, cost_of_path, nodes_expanded, fringe_size, max_fringe_size,
-                     search_depth, max_search_depth, runtime, max_ram_usage)
-            return
-
-        # the node out of queue is not a goal and we need to expand it
-        nodes_expanded += 1
-        #print(nodes_expanded)
-
-        for neighbor in node.neighbors():
-
-            if not (neighbor in explored_states or frontier.has_task(neighbor)):
-                frontier.add_task(neighbor, neighbor.path_cost + neighbor. manhattan_dis())
-                max_fringe_size = max(max_fringe_size, frontier.size())
-                max_search_depth = max(max_search_depth, neighbor.path_cost)
-            elif frontier.has_task(neighbor):
-                frontier.remove_task(neighbor)
-                frontier.add_task(neighbor, neighbor.path_cost + neighbor. manhattan_dis())
-
-    print("ERROR: ast should never reach this point")
-
-
-def ida_search(node, bound, st_time, nodes_expanded, max_fringe_size, max_search_depth):
-
-    # to keep the minum of values that crossed current bound
-    min_above_bound = float('inf')
-
-    frontier = deque([node])
-    # to have efficinet way to check for existence of a state in frontier
-    visited_states = set()
-    visited_states.add(node)
-
-    while len(frontier) > 0:
-        node = frontier.pop()
-
-        if node.is_goal_state():
-            # return (path to goal, cost_of_path, nodes expanded, fringe_size
-            path_to_goal = []
-            cur_state = node
-            while cur_state.parent:
-                path_to_goal.append(cur_state.move_frm_parent)
-                cur_state = cur_state.parent
-            path_to_goal = path_to_goal[::-1]
-            cost_of_path = len(path_to_goal)
-            fringe_size = len(frontier)
-            search_depth = node.path_cost
-            runtime = time.clock() - st_time
-            if not os.name == 'nt':
-                import resource
-                max_ram_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss # get ru_maxrss
-            else:
-                max_ram_usage = None
-
-            write_output(path_to_goal, cost_of_path, nodes_expanded, fringe_size, max_fringe_size,
-                     search_depth, max_search_depth, runtime, max_ram_usage)
-
-            return 'FOUND', bound, nodes_expanded, max_fringe_size, max_search_depth
-
-        # the node out of queue is not a goal and we need to expand it
-        nodes_expanded += 1
-        #print(nodes_expanded)
-
-        ## for dfs we need to reverse the neighbour list before inserstion so
-        # that popping will result in UDLR order
-        for neighbor in node.neighbors()[::-1]:
-
-            if not neighbor in visited_states:
-                cost = neighbor.path_cost + neighbor.manhattan_dis()
-                if  cost <= bound:
-                    frontier.append(neighbor)
-                    visited_states.add(neighbor)
-                    max_fringe_size = max(max_fringe_size, len(frontier))
-                    max_search_depth = max(max_search_depth, neighbor.path_cost)
-                else:
-                    min_above_bound = min(min_above_bound, cost)
-
-    return 'NOT_FOUND', min_above_bound, nodes_expanded, max_fringe_size, max_search_depth
-
-
-def ida(initial_state):
-
-    # start time
-    st_time = time.clock()
-    bound = initial_state.manhattan_dis()
-    nodes_expanded = 0
-    max_fringe_size = -1
-    max_search_depth = -1
-
-    while True:
-        status, bound, nodes_expanded, max_fringe_size, max_search_depth = \
-            ida_search(initial_state, bound, st_time, nodes_expanded, max_fringe_size, max_search_depth)
-
-        if status == 'FOUND':
-            break
-
-    print "IDA SOlved the problem"
 
 def write_output(path_to_goal, cost_of_path, nodes_expanded, fringe_size,
                  max_fringe_size, search_depth, max_search_depth, runtime,
@@ -433,7 +294,7 @@ def read_command( argv ):
 
 if __name__ == '__main__':
     args = read_command(sys.argv[1:])
-    state = State(args.state, None, None, 0)
+    state = State(args.state, None, None)
     method = args.method
     GOAL = tuple([i for i in range(len(state.state))])
     if method == 'bfs':
@@ -442,7 +303,4 @@ if __name__ == '__main__':
         dfs(state)
     elif method == 'ast':
         ast(state)
-    elif method == 'ida':
-        ida(state)
-
 
